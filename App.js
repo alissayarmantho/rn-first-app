f085f0715e694c4b6cd105d822224d00b0393ea3
Alissa <alissayarmantho1@gmail.com>
2020-10-11 20:13:43 +0800

    Updated

--- App.js
+++ App.js
@@ -35,6 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 30,
+    padding: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
